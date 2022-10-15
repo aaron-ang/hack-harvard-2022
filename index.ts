1ab59eabc4e0b5a105c13e7b52e7c3be3aae0cfd
Aaron Ang <67321817+aaron-ang@users.noreply.github.com>
2022-10-15 13:16:20 -0400

    some touble with await

--- index.ts
+++ index.ts
@@ -1,19 +1,45 @@
+import dotenv from "dotenv";
+import fs from "fs";
 import axios from "axios";
+import ytdl from "ytdl-core";
+dotenv.config();
 
-const APIKey = process.env.ASSEMBLYAI_API_KEY;
-const audioURL = "https://youtu.be/WO7wT-FX2mA";
+const APIKey = process.env.ASSEMBLY_API_KEY;
+const audioSource = "https://youtu.be/WO7wT-FX2mA";
+const audioFile = "audio.mp3";
+let audioURL = "";
 
 const assembly = axios.create({
   baseURL: "https://api.assemblyai.com/v2",
   headers: {
     authorization: APIKey,
     "content-type": "application/json",
+    "transfer-encoding": "chunked",
   },
 });
+
+// Need to pipe it to api call instead of saving locally
+await ytdl("https://youtu.be/NWGzAwqfOG4", { filter: "audioonly" }).pipe(
+  fs.createWriteStream(audioFile)
+);
+
+await fs.readFile(audioFile, (err, data) => {
+  if (err) return console.error(err);
+
+  assembly
+    .post("/upload", data)
+    .then((res) => {
+      console.log(res.data);
+      audioURL = res.data.upload_url;
+    })
+    .catch((err) => console.error(err));
+});
+
+console.log(`audio URL: ${audioURL}`);
 
 assembly
   .post("/transcript", {
     audio_url: audioURL,
   })
-  .then((res: { data: any }) => console.log(res.data))
-  .catch((err: any) => console.error(err));
+  .then((res) => console.log(res.data))
+  .catch((err) => console.error(err));
